52a33243f9d5f1b2b6ebd76dd9a1f5ee83da2b6a
neyneyalldayday <drfrankenstein123@gmail.com>
2024-08-15 17:53:08 -0500

    created a contact form

--- Client/src/pages/Contact/index.jsx
+++ Client/src/pages/Contact/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const formRef = useRef();
+    useEffect(() => {
+        // Initialize EmailJS with your user ID
+        emailjs.init('iJaR9zspjdomC9XL2');
+    }, []);
+    const sendEmail = (e) => {
+      e.preventDefault();
+        
+   
+
+      emailjs.sendForm('service_yd9p1np', 'template_pbzaykh', formRef.current)
+        .then((result) => {
+            console.log("your message was sent daddy ",result.text);
+            formRef.current.reset();
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+
   return (
-    <div>Contact</div>
+    <>
+    <form ref={formRef} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+
+      <label>Email</label>
+      <input type="email" name="user_email" />
+
+      <label>Message</label>
+      <textarea name="message" />
+
+      <input type="submit" value="Send" />
+    </form>
+    </>
   )
 }
 
--- Client/src/pages/Hero/index.jsx
+++ Client/src/pages/Hero/index.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='hero-container'>
       <img id="hero" src={image} alt="" />
       <div className='hero-content'>
-      <h1><span class="animated-char">L</span><span class="animated-char">e</span><span class="animated-char">P</span><span class="animated-char">a</span><span class="animated-char">l</span><span class="animated-char">e</span><span class="animated-char">f</span><span class="animated-char">o</span><span class="animated-char">x</span></h1>
+      <h1><span className="animated-char">L</span><span className="animated-char">e</span><span className="animated-char">P</span><span className="animated-char">a</span><span className="animated-char">l</span><span className="animated-char">e</span><span className="animated-char">f</span><span className="animated-char">o</span><span className="animated-char">x</span></h1>
       </div>
     </div>
   )
