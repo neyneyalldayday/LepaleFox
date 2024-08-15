2fc621e62960f28a2cec530b5a8ce8c298bc971d
neyneyalldayday <drfrankenstein123@gmail.com>
2024-08-15 17:12:39 -0500

    hero page sorta

--- Client/src/pages/Hero/index.jsx
+++ Client/src/pages/Hero/index.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import image from '../../assets/image.jpg'
 import './hero.css'
 const Hero = () => {
   return (
-    <div className='hero-container'>Hero!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
+    <div className='hero-container'>
+      <img id="hero" src={image} alt="" />
+      <div className='hero-content'>
+      <h1><span class="animated-char">L</span><span class="animated-char">e</span><span class="animated-char">P</span><span class="animated-char">a</span><span class="animated-char">l</span><span class="animated-char">e</span><span class="animated-char">f</span><span class="animated-char">o</span><span class="animated-char">x</span></h1>
+      </div>
+    </div>
   )
 }
 
