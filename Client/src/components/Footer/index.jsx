881672e6c1505a4a2fbdf520c1e167c3d05ac8f7
neyneyalldayday <drfrankenstein123@gmail.com>
2024-08-16 17:41:17 -0500

    added a gallery from an event

--- Client/src/components/Footer/index.jsx
+++ Client/src/components/Footer/index.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebookF, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import './footer.css'
+import event from '../../assets/event.jpg'
+import event2 from '../../assets/event2.jpg'
+import event3 from '../../assets/event3.jpg'
+import event4 from '../../assets/event4.jpg'
+import event5 from '../../assets/event5.jpg'
+import event6 from '../../assets/event6.jpg'
 const Footer
  = () => {
   return (
@@ -16,6 +22,26 @@ const Footer
         <a href="https://www.tiktok.com/@your_tiktok_username" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTiktok} /> 
         </a>
+      </div>
+      <div className='galery'>
+        <div className='image-container'>
+          <img className='event-image' src={event} alt="" />
+        </div>
+        <div className='image-container'>
+          <img className='event-image' src={event2} alt="" />
+        </div>
+        <div className='image-container'>
+          <img className='event-image' src={event3} alt="" />
+        </div>
+        <div className='image-container'>
+          <img className='event-image' src={event4} alt="" />
+        </div>
+        <div className='image-container'>
+          <img className='event-image' src={event5} alt="" />
+        </div>
+        <div className='image-container'>
+          <img className='event-image' src={event6} alt="" />
+        </div>
       </div>
       <p>&copy; 2024 LePalefox. All rights reserved.</p>
     </footer>
