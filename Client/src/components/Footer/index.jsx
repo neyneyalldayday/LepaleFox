3861f0b1f9f821cca3ce50323558df9fa6e20edf
neyneyalldayday <drfrankenstein123@gmail.com>
2024-10-01 14:36:20 -0500

    removed photos from footer.

--- Client/src/components/Footer/index.jsx
+++ Client/src/components/Footer/index.jsx
@@ -2,33 +2,9 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebookF, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import './footer.css'
-import event from '../../assets/event.jpg'
-import event2 from '../../assets/event2.jpg'
-import event3 from '../../assets/event3.jpg'
-import event4 from '../../assets/event4.jpg'
-import event5 from '../../assets/event5.jpg'
-import event6 from '../../assets/event6.jpg'
 
-const eventArray = [
-  {
-    imageUrl : event
-  },
-  {
-    imageUrl : event2
-  },
-  {
-    imageUrl : event3
-  },
-  {
-    imageUrl : event4
-  },
-  {
-    imageUrl : event5
-  },
-  {
-    imageUrl : event6
-  },
-]
+
+
 
 
 const Footer = () => {
@@ -45,13 +21,7 @@ const Footer = () => {
           <FontAwesomeIcon icon={faTiktok} /> 
         </a>
       </div>
-      <div className='galery'>
-        {eventArray.map((event, index) => (
-          <div key={index} className='image-container'>
-            <img className='event-image' src={event.imageUrl} alt="" />
-          </div>
-        ))}      
-      </div>
+   
       <p>&copy; 2024 LePalefox. All rights reserved.</p>
     </footer>
   )
