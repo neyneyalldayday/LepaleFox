aba4653e866081371f2a4e5c33134771b4c84dab
Rene Trevino <drfrankenstein123@gmail.com>
2025-04-02 17:51:30 -0500

    fan has their own log in and log out

---
+++ Server/controllers/api/fan-controller.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const {Fan} = require("../../models");
+
+
+
+router.post('/createfan', async (req, res) => {
+try {
+    const createFan = await Fan.create(req.body);
+
+    req.session.save(() => {
+        req.session.user_id = createFan.id;
+        req.session.username = createFan.username;
+        req.session.logged_in = true;
+
+
+        res.status(200).json({createFan, message:"you are now logged in"})
+    })
+} catch (err) {
+    console.error(err);
+    res.status(500).json(err)
+}
+})
+
+
+router.post('/letinFan', async (req, res)=> {
+    try {
+      const letInFan = await Fan.findOne({
+        where: {username: req.body.username},
+      })
+
+      if (!letInFan) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+
+      const validPass = await letInFan.checkPassword(req.body.password);
+
+      if (!validPass){
+        res.status(400).json({message: 'incorrect user daddy'});
+        return;
+      }
+
+
+      req.session.save(() => {
+        req.session.user_id = letInFan.id;
+        req.session.username = letInFan.username;
+        req.session.logged_in = true;
+        
+  
+        res.status(200).json({
+          letInFan,
+          message: 'You are now logged in!',
+        });
+      });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err)
+        
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- Server/controllers/api/index.js
+++ Server/controllers/api/index.js
@@ -4,13 +4,15 @@ const btownRoutes = require('./btown-controller');
 const postRoutes = require('./post-controller');
 const commentRoutes = require('./comment-controller');
 const replyRoutes = require('./reply-controller');
-const uploadRoutes = require('./photo-controller')
+const uploadRoutes = require('./photo-controller');
+const fanRoutes = require('./fan-controller');
 
 router.use('/btown', btownRoutes);
 router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
 router.use('/reply', replyRoutes);
 router.use('/upload', uploadRoutes);
+router.use('/fan', fanRoutes);
 
 
 
